8c7482b1a0eeaf67835843a75363c9bb713030ca
Yura <Yura.arkhipov.2004@mail.ru>
2025-02-01 14:00:44 +0300

    [unit-2] TipTime tests

--- tipcalculator/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ tipcalculator/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tipcalculator.ui.theme.JetpackTutorialTheme
+import org.jetbrains.annotations.VisibleForTesting
 import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
@@ -107,7 +108,8 @@ fun TipTimeLayout() {
     }
 }
 
-private fun calculateTip(
+@VisibleForTesting
+internal fun calculateTip(
     amount: Double,
     tipPercent: Double = 15.0,
     roundUp: Boolean
